a0eb18b7f5636dcfe9d04da52f7ecd21b8df7202
tjd6356-afk <tjd6356@gmail.com>
2026-03-19 15:23:42 +0900

    20260319

--- my project01/Assets/player_controller.cs
+++ my project01/Assets/player_controller.cs
@@ -5,15 +5,30 @@ public class player_controller : MonoBehaviour
 {
     private Vector2 moveInput;
     public float moveSpeed = 7f;
+    public float jumpFoerce = 7f;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
 
-    public void OnMove(InputValue value)
-    {
-        moveInput = value.Get<Vector2>();
-    }
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
+
+     public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpFoerce);
+        }
     }
 
     // Update is called once per frame
@@ -26,6 +41,15 @@ public class player_controller : MonoBehaviour
         else if(moveInput.x < 0)
         {
             transform.localScale = new Vector3(-1,1,1);
+        }
+
+        if(moveInput.magnitude > 0)
+        {
+            myAnimator.SetBool("move", true);
+        }
+        else
+        {
+            myAnimator.SetBool("move", false);
         }
         transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
     }
